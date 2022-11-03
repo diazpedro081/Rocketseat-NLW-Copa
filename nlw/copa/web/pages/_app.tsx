e77593b163ab098e28d11a10afdfee5803fac11b
diazpedrodev <081diazpedro@gmail.com>
2022-11-03 15:01:22 -0300

    Correções Front-end

--- nlw/copa/web/pages/_app.tsx
+++ nlw/copa/web/pages/_app.tsx
@@ -1,4 +1,3 @@
-
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
--- nlw/copa/web/pages/index.tsx
+++ nlw/copa/web/pages/index.tsx
@@ -1,21 +1,21 @@
-interface homeProps {
+interface HomeProps {
   count: number;
 }
 
-export default function Home(props: homeProps) {
+export default function Home(props: HomeProps) {
   return (
-    <h1>Contagem: { props.count }</h1>
+    <h1>Contagem: {props.count}</h1>
   )
 }
 
-export const getServersideProps = async () => {
+export const getServerSideProps = async () => {
   const response = await fetch("http://localhost:3333/pools/count")
   const data = await response.json()
 
   return {
-    props: { 
+    props: {
       count: data.count,
-     }
+    }
   }
-  }
+}
 
