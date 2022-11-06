ebe79318db270951ae980090d178ca60d7794299
diazpedrodev <081diazpedro@gmail.com>
2022-11-06 13:26:23 -0300

    cfg

--- nlw/copa/mobile/App.tsx
+++ nlw/copa/mobile/App.tsx
@@ -1,24 +1,28 @@
-import { NativeBaseProvider, StatusBar } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from "@expo-google-fonts/roboto";
-
-import { SingIn } from "./src/screens/SingIn"
-import { Loading } from './src/components/Loading';
+import { NativeBaseProvider, StatusBar } from 'native-base'
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 import { THEME } from './src/styles/theme';
 
+import { Loading } from './src/components/Loading';
+import { AuthContextProvider } from './src/contexts/AuthContext';
+import { Routes } from './src/routes';
+
 export default function App() {
 
-  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? <SingIn /> : <Loading />}
+      <AuthContextProvider>
+        <StatusBar
+          barStyle='light-content'
+          backgroundColor="transparent"
+          translucent
+        />
+        {
+          fontsLoaded ? <Routes /> : <Loading />
+        } 
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
-
