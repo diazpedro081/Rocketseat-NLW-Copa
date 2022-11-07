9da82fe122a27bb009dd653cc882daa37a5e3bb5
diazpedrodev <081diazpedro@gmail.com>
2022-11-07 12:28:07 -0300

    Mobile cfg

--- nlw/copa/mobile/App.tsx
+++ nlw/copa/mobile/App.tsx
@@ -1,11 +1,11 @@
-import { NativeBaseProvider, StatusBar } from 'native-base'
-import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { NativeBaseProvider, StatusBar } from "native-base"
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from "@expo-google-fonts/roboto"
 
-import { THEME } from './src/styles/theme';
+import { THEME } from "./src/styles/theme";
 
-import { Loading } from './src/components/Loading';
-import { AuthContextProvider } from './src/contexts/AuthContext';
-import { Routes } from './src/routes';
+import { Loading } from "./src/components/Loading";
+import { AuthContextProvider } from "./src/contexts/AuthContext";
+import { Routes } from "./src/routes";
 
 export default function App() {
 
@@ -15,7 +15,7 @@ export default function App() {
     <NativeBaseProvider theme={THEME}>
       <AuthContextProvider>
         <StatusBar
-          barStyle='light-content'
+          barStyle="light-content"
           backgroundColor="transparent"
           translucent
         />
