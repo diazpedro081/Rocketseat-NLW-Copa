15febdb8dc6413f84827e9a1c4282fcac9a6e61e
diazpedrodev <081diazpedro@gmail.com>
2022-11-07 11:01:09 -0300

    server main

--- nlw/copa/server/src/server.ts
+++ nlw/copa/server/src/server.ts
@@ -1,27 +1,34 @@
-import Fastify from "fastify";
+import Fastify from "fastify"
+import jwt from "@fastify/jwt"
 import cors from "@fastify/cors"
-import { poolRooutes } from "./routes/pool";
-import { userRooutes } from "./routes/user";
-import { guessRooutes } from "./routes/guess";
-import { authRooutes } from "./routes/auth";
-import { gameRooutes } from "./routes/game";
+import { authRoutes } from "./routes/auth"
+import { userRoutes } from "./routes/users.routes"
+import { poolRoutes } from "./routes/pools.routes"
+import { guessesRoutes } from "./routes/guesses.routes"
+import { gameRoutes } from "./routes/games.routes"
+
+
 
 async function bootstrap() {
-    const fastify = Fastify({
-        logger: true
-    })
+  const fastify = Fastify({
+    logger: true,
+  })
 
-    await fastify.register(cors, {
-        origin: true,
-    })
-    
-    await fastify.register(authRooutes)
-    await fastify.register(gameRooutes)
-    await fastify.register(guessRooutes)
-    await fastify.register(poolRooutes)
-    await fastify.register(userRooutes)
+  await fastify.register(cors, {
+    origin: true,
+  })
 
-    await fastify.listen({ port: 3333,/* host: '0.0.0.0' */ })
+  await fastify.register(jwt, {
+    secret: "nlwcopa"
+  })
+
+  await fastify.register(authRoutes)
+  await fastify.register(userRoutes)
+  await fastify.register(poolRoutes)
+  await fastify.register(guessesRoutes)
+  await fastify.register(gameRoutes)
+
+  await fastify.listen({ port: 3333, host: "0.0.0.0" })
 }
 
 bootstrap()
